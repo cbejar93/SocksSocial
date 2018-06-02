86b6d3e745438f20145f2a6af49d2d04acf3939f
unknown <cbejar93@gmail.com>
2018-06-02 18:52:13 -0400

    rain

---
+++ create-react-express/client/src/pages/Home/Home.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+    constructor (){
+        super();
+        this.state = {
+            socks: [],
+            user: [],
+            title: "",
+        }
+    }
+
+    render(){
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+
+
+export default Home;
