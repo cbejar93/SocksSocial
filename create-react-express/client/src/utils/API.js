b0357106b779f371253f8332f112de148613cc52
unknown <cbejar93@gmail.com>
2018-06-02 14:49:59 -0400

    api

--- create-react-express/client/src/utils/API.js
+++ create-react-express/client/src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     createUser: function (body) {
-        return axios.post("api/signup", body);
+        return axios.post("api/users", body);
     }
 }
